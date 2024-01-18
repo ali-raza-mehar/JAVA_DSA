3dcf0b8cbb748a1823849ba14a9a977f77b1ddd5
ali-raza-mehar <contact-to-ali-raza@gmail.com>
2024-01-18 22:55:21 +0500

    initial code

--- Initial Code/08-Array/Assignments/Maximum Population Year/Problem.java
+++ Initial Code/08-Array/Assignments/Maximum Population Year/Problem.java
@@ -2,40 +2,24 @@
 public class Problem {
     public static void main(String[] args) {
         int[][] logs = {
-                { 2033, 2034 },
-                { 2039, 2047 },
-                { 1998, 2042 },
-                { 2047, 2048 },
-                { 2025, 2029 },
-                { 2005, 2044 },
-                { 1990, 1992 },
-                { 1952, 1956 },
-                { 1984, 2014 }
+                { 1993, 1999 },
+                { 2000, 2010 }
         };
         System.out.println(maximumPopulation(logs));
     }
 
     static int maximumPopulation(int[][] logs) {
-        int[] array;
-        int loopForarray = 0;
-        int startBirthYear = logs[0][0];
+        int[] array = new int[101];
         for (int i = 0; i < logs.length; i++) {
-            loopForarray = loopForarray + (logs[i][1] - logs[i][0]);
-            if (logs[i][0] < startBirthYear)
-                startBirthYear = logs[i][0];
-        }
-        array = new int[loopForarray];
-        for (int i = 0; i < logs.length; i++) {
-            for (int j = startBirthYear; j < logs[i][1]; j++) {
-                array[j - startBirthYear]++;
-            }
+            for (int j = logs[i][0]; j < logs[i][1]; j++)
+                array[j - 1950]++;
         }
         int maxValue = 0;
-        int maxYear = 0;
-        for (int i = 0; i < loopForarray; i++) {
+        int maxYear = 1950;
+        for (int i = 0; i < 101; i++) {
             if (array[i] > maxValue) {
                 maxValue = array[i];
-                maxYear = i + logs[0][0];
+                maxYear = i + 1950;
             }
         }
         return maxYear;
