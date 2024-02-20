8343b09d565646de9b40f8402d7974ab61ca2e5b
ali-raza-mehar <contact.to.ali.raza@gmail.com>
2024-02-20 10:04:06 +0500

    initial code

--- Initial Code/08-Array/Assignments/Easy/Plus One/Plus_One.java
+++ Initial Code/08-Array/Assignments/Easy/Plus One/Plus_One.java
@@ -1,32 +1,28 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
-public class Plus_One {
+public class Plus_one {
+
+    static int[] plusOne(int[] digits) {
+
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                break;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        if (digits[0] == 0) {
+            int[] array = new int[n + 1];
+            array[0] = 1;
+            return array;
+        }
+        return digits;
+    }
+
     public static void main(String[] args) {
-        int[] num;
-        int num2;
-        num = new int[3];
-        Scanner in = new Scanner(System.in);
-        for (int i = 0; i < 3; i++) {
-            System.out.print("Enter three digits in array one by one: ");
-            num[i] = in.nextInt();
-        }
-        String str1 = "";
-        for (int i = 0; i < 3; i++) {
-            str1 = str1 + num[i];
-        }
-        str1 = str1.trim();
-        num2 = Integer.parseInt(str1);
-        num2 = num2 + 1;
-        str1 = Integer.toString(num2);
-        int lastIndex = str1.length() - 1;
-        String[] str2;
-        str2 = new String[lastIndex];
-        for (int i = 0; i <= lastIndex; i++) {
-            str2[i] = String.valueOf(str1.charAt(i));
-        }
-        for (int i = 0; i < str2.length; i++) {
-            System.out.print(str2[i]);
-        }
-        in.close();
+        int[] array = { 9, 9, 9 };
+        System.out.println(Arrays.toString(plusOne(array)));
     }
 }
