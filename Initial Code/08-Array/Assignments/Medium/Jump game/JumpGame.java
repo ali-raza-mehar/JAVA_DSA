bae1c2c52a4e2c6957038234811fc76c128b298e
ali-raza-mehar <contact.to.ali.raza@gmail.com>
2024-03-30 21:12:50 +0500

    initial code

--- Initial Code/08-Array/Assignments/Medium/Jump game/JumpGame.java
+++ Initial Code/08-Array/Assignments/Medium/Jump game/JumpGame.java
@@ -1,42 +1,18 @@
 
 public class JumpGame {
     static boolean jump_Game(int[] nums) {
-        int lastIndex = nums.length - 1;
-        if (nums.length == 1 && nums[nums.length - 1] != 0) {
+
+        int goal = nums.length - 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+        if (goal == 0) {
             return true;
-        }
-        if (nums.length > 1) {
-            if (nums[0] == 0)
-                return false;
-        }
-        // if(nums[0] == 0)
-        // return false;
-        for (int i = 0; i <= lastIndex;) {
-            int currentIndex = i;
-            i += nums[i];
-            if (i >= lastIndex) {
-                return true;
-            }
-            for (int j = currentIndex + 1; j < i; j++) {
-                if (j + nums[j] >= lastIndex)
-                    return true;
-                if (j + nums[j] == i + 1)
-                    i = j + nums[j];
-            }
-            if (nums[i] == 0) {
-                for (int j = currentIndex + 1; j < i;) {
-                    j += nums[j];
-                    if (j >= lastIndex)
-                        return true;
-                    if (nums[j] == 0)
-                        return false;
-                    if (j == i + 1 && nums[j] != 0)
-                        i = j;
-                }
-                return false;
-            }
-        }
-        return false;
+        } else
+            return false;
+
     }
 
     public static void main(String[] args) {
---
+++ Initial Code/08-Array/Assignments/Medium/Rotate Array/Rotate_Array.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Rotate_Array {
+
+    static void rotate(int[] nums, int k) {
+
+        int lastIndex = nums.length - 1;
+        for (int i = 0; i < k; i++) {
+            int temp = nums[lastIndex];
+            for (int j = nums.length - 2; j >= 0; j--) {
+                nums[i + 1] = nums[i];
+            }
+            nums[0] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+        rotate(array, 3);
+        System.out.println(Arrays.toString(array));
+    }
+
+}
