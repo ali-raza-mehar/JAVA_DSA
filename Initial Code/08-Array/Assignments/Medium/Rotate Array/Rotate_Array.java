26898ff5f89b4a01ef08653d0594823d0236726e
ali-raza-mehar <contact.to.ali.raza@gmail.com>
2024-04-01 22:37:13 +0500

    initial code

--- Initial Code/08-Array/Assignments/Medium/Rotate Array/Rotate_Array.java
+++ Initial Code/08-Array/Assignments/Medium/Rotate Array/Rotate_Array.java
@@ -3,19 +3,43 @@ import java.util.Arrays;
 public class Rotate_Array {
 
     static void rotate(int[] nums, int k) {
-
-        int lastIndex = nums.length - 1;
-        for (int i = 0; i < k; i++) {
-            int temp = nums[lastIndex];
-            for (int j = nums.length - 2; j >= 0; j--) {
-                nums[j + 1] = nums[j];
-            }
-            nums[0] = temp;
+        int n = nums.length;
+        /* firstly reverse the entire array */
+        int left = 0, right = n - 1;
+        // int mid = n / 2;
+        for (; left < right; left++) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            right--;
         }
+        // reverse the k elements
+        left = 0;
+        right = k - 1;
+        // mid = k / 2;
+        for (; left < right; left++) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            right--;
+        }
+
+        // now reverse the others elements
+        left = k;
+        right = n - 1;
+        for (; left < right; left++) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            right--;
+        }
+
     }
 
     public static void main(String[] args) {
         int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+        // int[] array = { 1, 2 };
+        // int[] array = { -1, -100, 3, 99 };
         rotate(array, 3);
         System.out.println(Arrays.toString(array));
     }
