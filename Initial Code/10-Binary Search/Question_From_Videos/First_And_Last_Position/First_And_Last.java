7ca238a17ad76ff2f11c8fb75a1d6fa5bcf2e716
ali-raza-mehar <contact.to.ali.raza@gmail.com>
2024-04-23 11:21:11 +0500

    initial code

--- Initial Code/10-Binary Search/Question_From_Videos/First_And_Last_Position/First_And_Last.java
+++ Initial Code/10-Binary Search/Question_From_Videos/First_And_Last_Position/First_And_Last.java
@@ -1,54 +1,35 @@
 package Question_From_Videos.First_And_Last_Position;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class First_And_Last {
-    static int[] positions(int[] nums, int target) {
+    static int[] searchRange(int[] nums, int target) {
         int[] ans = { -1, -1 };
+        int start = search(nums, target, true);
+        int end = search(nums, target, false);
+        ans[0] = start;
+        ans[1] = end;
+        return ans;
+    }
+
+    static int search(int[] nums, int target, boolean startPointFinding) {
+        int ans = -1;
         int sp = 0;
         int ep = nums.length - 1;
-        int count = 1;
-        int initialMid = 0;
         while (sp <= ep) {
             int mid = sp + (ep - sp) / 2;
 
-            if (nums[mid] != target) {
-                if (target > nums[mid]) {
-                    sp = mid + 1;
-                } else if (target < nums[mid]) {
+            if (target > nums[mid]) {
+                sp = mid + 1;
+            } else if (target < nums[mid]) {
+                ep = mid - 1;
+            } else {
+                // update the potential ans
+                ans = mid;
+                if (startPointFinding) {
                     ep = mid - 1;
-                }
-            } else if (nums[mid] == target) {
-                if (nums[mid - 1] != target && nums[mid + 1] != target) {
-                    // it means element appear only once
-                    ans[0] = mid;
-                    ans[1] = mid;
-                    return ans;
-                } else if (nums[mid - 1] == target && nums[mid + 1] != target) {
-                    ans[1] = mid - 1;
-                    ep = mid - 1;
-                } else if (nums[mid - 1] != target && nums[mid + 1] == target) {
-                    ans[0] = mid;
+                } else
                     sp = mid + 1;
-                } else if (nums[mid + 1] == target && nums[mid - 1] == target) {
-                    initialMid = mid;
-                    count = 1;
-                    ep = mid - 1;
-                }
-            }
-        }
-        if (count == 1) {
-            sp = initialMid + 1;
-            ep = nums.length - 1;
-            while (sp <= ep) {
-                int mid = sp + (ep - sp) / 2;
-
-                if (nums[mid + 1] == target) {
-                    sp = mid + 1;
-                } else if (nums[mid + 1] != target) {
-                    ans[1] = mid;
-                }
             }
         }
         return ans;
@@ -56,8 +37,7 @@ public class First_And_Last {
 
     public static void main(String[] args) {
         int[] nums = { 5, 6, 6, 6, 7, 8, 8, 9, 10 };
-        int[] ans = positions(nums, 6);
-        System.out.println(ans[0]);
-        System.out.println(ans[1]);
+        int[] ans = searchRange(nums, 7);
+        System.out.println(Arrays.toString(ans));
     }
 }
---
+++ Initial Code/10-Binary Search/Question_From_Videos/First_Position_Of_Element_In_Sorted_Infinite_Array/First_Position.java
@@ -0,0 +1,5 @@
+package Question_From_Videos.First_Position_Of_Element_In_Sorted_Infinite_Array;
+
+public class First_Position {
+
+}
