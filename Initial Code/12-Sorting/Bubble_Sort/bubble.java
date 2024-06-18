2bd13aeb0c91166c5b227206814b0d77da7682f6
ali-raza-mehar <contact.to.ali.raza@gmail.com>
2024-06-18 13:19:27 +0500

    initial code

---
+++ Initial Code/12-Sorting/Bubble_Sort/bubble.java
@@ -0,0 +1,40 @@
+
+package Bubble_Sort;
+
+import java.util.Arrays;
+
+public class bubble {
+
+    static void sort(int[] nums) {
+        int n = nums.length;
+        boolean swaped;
+        // check swaped for ith pass
+        // if there is no swap in any ith swap
+        // then break the loop
+        for (int i = 1; i < n; i++) {
+            swaped = false;
+            for (int j = 1; j <= n - i; j++) {
+                if (nums[j - 1] > nums[j]) {
+                    swap(nums, j - 1, j);
+                    swaped = true;
+                }
+            }
+            if (!swaped) {
+                break;
+            }
+        }
+        // return nums;
+    }
+
+    static void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4 };
+        sort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
