f29ec13c6c5d291dac894bb56d5f60963619d9a0
ali-raza-mehar <contact.to.ali.raza@gmail.com>
2024-11-09 13:57:08 +0500

    initial code

---
+++ Initial Code/15- Recursion/String_Question/SkipA.java
@@ -0,0 +1,22 @@
+package String_Question;
+
+public class SkipA {
+    static StringBuilder sb = new StringBuilder();
+
+    static String remove(String s, int i) {
+        if (i == s.length()) {
+            return sb.toString();
+        }
+        if (s.charAt(i) != 'a' && s.charAt(i) != 'A') {
+            sb.append(s.charAt(i) + "");
+            return remove(s, i + 1);
+        } else {
+            return remove(s, i + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "baccad";
+        System.out.println(remove(s, 0));
+    }
+}
