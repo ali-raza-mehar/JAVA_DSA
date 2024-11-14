aa4064cb9fa3e23a93ca26c1e4613c79b6d22bdc
ali-raza-mehar <contact.to.ali.raza@gmail.com>
2024-11-14 09:24:08 +0500

    initial code

--- Initial Code/15- Recursion/String_Question/SkipA.java
+++ Initial Code/15- Recursion/String_Question/SkipA.java
@@ -1,17 +1,30 @@
 package String_Question;
 
 public class SkipA {
-    static StringBuilder sb = new StringBuilder();
 
-    static String remove(String s, int i) {
+    static String remove(String s, int i, StringBuilder sb) {
         if (i == s.length()) {
             return sb.toString();
         }
         if (s.charAt(i) != 'a' && s.charAt(i) != 'A') {
             sb.append(s.charAt(i) + "");
-            return remove(s, i + 1);
+            return remove(s, i + 1, sb);
         } else {
-            return remove(s, i + 1);
+            return remove(s, i + 1, sb);
+        }
+    }
+
+    // by putting String in argument
+    static void skip(String a, String res) {
+        if (a.isEmpty()) {
+            System.out.println(res);
+            return;
+        }
+        char ch = a.charAt(0);
+        if (ch != 'a' && ch != 'A') {
+            skip(a.substring(1), res + ch);
+        } else {
+            skip(a.substring(1), res);
         }
     }
 
@@ -19,6 +32,8 @@ public class SkipA {
 
     public static void main(String[] args) {
         String s = "baccad";
-        System.out.println(remove(s, 0));
+        System.out.println(remove(s, 0, new StringBuilder()));
+        // System.out.println(remove(s, 0));
+        // skip(s, "");
     }
 }
---
+++ Initial Code/15- Recursion/String_Question/SkipString.java
@@ -0,0 +1,20 @@
+package String_Question;
+
+public class SkipString {
+    static String skipString(String s) {
+        if (s.isEmpty()) {
+            return "";
+        }
+        // if start with apple then
+        // skip those five characters
+        if (s.startsWith("apple")) {
+            return skipString(s.substring(5));
+        }
+        return s.charAt(0) + skipString(s.substring(1));
+    }
+
+    public static void main(String[] args) {
+        String s = "applecdef";
+        System.out.println(skipString(s));
+    }
+}
